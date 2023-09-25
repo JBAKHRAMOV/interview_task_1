11515c0377cc2e1f2e623ce6971f3f9decae49b0
j_bakhramov <bahromovjamshid503@gmail.com>
2023-09-25 22:55:32 +0500

    start

---
+++ src/main/java/com/compny/IterviewTaskApplication.java
@@ -0,0 +1,13 @@
+package com.compny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterviewTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IterviewTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compny/IterviewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterviewTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
